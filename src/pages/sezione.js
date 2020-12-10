678a1380ccaf3b972957925da468eaeba5930262
magiaslab <social@magiaslab.com>
2020-12-10 23:26:45 +0100

    prova

---
+++ src/pages/sezione.js
@@ -0,0 +1,17 @@
+import React from "react"
+import {
+    link
+} from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const sezione = () => ( <
+    Layout >
+    <
+    SEO title = "sezione" / >
+
+    <h1>Titolo della sezione</h1>
+    </Layout>
+)
