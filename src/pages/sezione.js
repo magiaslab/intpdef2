466bb8966e378035096ede3f7ae99a7f94ccdd4a
magiaslab <social@magiaslab.com>
2020-12-10 23:35:20 +0100

    link

--- src/pages/sezione.js
+++ src/pages/sezione.js
@@ -1,10 +1,7 @@
 import React from "react"
-import {
-    link
-} from "gatsby"
+
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const sezione = () => ( <
